f55d0a483bd087d58ad8744103a8ac5a8dbd9784
Facundo Ramos <facundo@wepay.com>
2017-09-09 15:08:21 -0700

    Update gitignore fix homepage path

--- app/controller/home.py
+++ app/controller/home.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 home = Blueprint('home', __name__)
 
 
-@home.route('/index')
+@home.route('/')
 def index():
     return render_template('home/index.html')
