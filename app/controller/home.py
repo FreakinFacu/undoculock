0c03e316525a785859ab9b55d4cdaa436d3190fb
Facundo Ramos <facundo@wepay.com>
2017-09-09 16:18:22 -0700

    Added simple facebook login button with no callback

--- app/controller/home.py
+++ app/controller/home.py
@@ -6,3 +6,8 @@ home = Blueprint('home', __name__)
 @home.route('/')
 def index():
     return render_template('home/index.html')
+
+
+@home.route('/fb')
+def fb():
+    return render_template('home/fb.html')
