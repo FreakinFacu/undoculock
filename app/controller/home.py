65dfaf45e2a978e8e7511a730563d9596bf25875
Facundo Ramos <facundo@wepay.com>
2017-09-09 23:20:14 -0700

    clean home

--- app/controller/home.py
+++ app/controller/home.py
@@ -6,15 +6,18 @@ home = Blueprint('home', __name__)
 @home.route('/')
 def index():
     return render_template('home/index.html')
-<<<<<<< HEAD
+
+
+@home.route('/data')
 def data():
     return render_template('home/data.html')
+
+
+@home.route('/info')
 def info():
     return render_template('home/info.html')
-=======
 
 
 @home.route('/fb')
 def fb():
     return render_template('home/fb.html')
->>>>>>> origin/master
