5a1147fcc2fd325352259c8b8487a42d6180823e
zer0dev <dwebcrew@protonmail.com>
2017-09-10 09:54:36 -0700

    my shity code

--- static/js/xdata.js
+++ static/js/xdata.js
@@ -1,32 +1,52 @@
-//$(document).ready(function(){
-//    $('#u_like').change(function(){
-//        if($(this).prop('checked') === true) {
-////            if the switch is turn onn then put code here!
-//            Materialize.toast('activated',3000);
-//            var user_likes = prompt("please enter the number of likes");
-//
-////            ## if cancell, change the value to dissable
-//            if (user_likes === null){
-////                This string will grab the current value
-//                _val = $('#u_like').value;
-//                x = _val ? false : true;
-//                if (x === true){
-//                    console.log("Cancelled");
-//                }else{
-//                    console.log("null error on like switch");
-//                };
-//
-//            }else if (user_likes === ""){
-//                alert("you forgot to select a number, the default number of 100 likes will be selected. ");
-//                user_likes = 100;
-//                console.log(" user has selected the default option of " + user_likes +" likes");
-//            } else {
-//                console.log("user selected "+ user_likes + " likes")
-//            };
-//
-//        }else{
-////            if switch is deactivated then put code here!
-//            Materialize.toast('deactivated', 3000);
-//        }
-//    });
-//});
+//// SFACEBOOK MODEULE ////
+var prev_val;
+$(document).ready(function () {
+    $('#s_fb').click(function () {
+        prev_val = $(this).val();
+    }).change(function () {
+        $(this).unbind('click');
+        if ($(this).prop('checked') === true) {
+            //            if the switch is turn onn then put code here!
+            //            Materialize.toast('Activating share via facebook',3000);
+            var user_fb = prompt("Please Enter the user name");
+
+            //            ## if cancell, change the value to dissable
+            if (user_fb === null) {
+                //                This string will grab the current value
+                _val = $('#s_fb').value;
+                x = _val ? false : true;
+                if (x === true) {
+                    console.log("Cancelled");
+                } else {
+                    console.log("null error on like switch");
+                };
+
+            } else if (user_fb === "") {
+                alert("You have not entered a valid email. ");
+                $(this).val(prev_val);
+                $(this).bind('click');
+                return false;
+            } else {
+                console.log("user selected " + user_fb)
+                Materialize.toast('Activating share via facebook', 3000);
+            };
+
+        } else {
+            //            if switch is deactivated then put code here!
+            Materialize.toast('Facebook shared has been deactivated', 3000);
+        }
+    });
+});
+
+
+//// SHARE EMAIL MODULE ////
+
+$(document).ready(function () {
+    $('#s_email').change(function () {
+        if ($(this).prop('checked') === true) {
+            Materialize.toast('Activating share via email', 3000);
+            let user_input
+        }
+
+    })
+})
