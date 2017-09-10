1d3c65903b4cc83bc5500c6362e4b1a0d5b23f7c
zer0dev <dwebcrew@protonmail.com>
2017-09-10 02:58:31 -0700

    fuck

---
+++ static/js/xdata.js
@@ -0,0 +1,32 @@
+//$(document).ready(function(){
+//    $('#u_like').change(function(){
+//        if($(this).prop('checked') === true) {
+////            if the switch is turn onn then put code here!
+//            Materialize.toast('activated',3000);
+//            var user_likes = prompt("please enter the number of likes");
+//
+////            ## if cancell, change the value to dissable
+//            if (user_likes === null){
+////                This string will grab the current value
+//                _val = $('#u_like').value;
+//                x = _val ? false : true;
+//                if (x === true){
+//                    console.log("Cancelled");
+//                }else{
+//                    console.log("null error on like switch");
+//                };
+//
+//            }else if (user_likes === ""){
+//                alert("you forgot to select a number, the default number of 100 likes will be selected. ");
+//                user_likes = 100;
+//                console.log(" user has selected the default option of " + user_likes +" likes");
+//            } else {
+//                console.log("user selected "+ user_likes + " likes")
+//            };
+//
+//        }else{
+////            if switch is deactivated then put code here!
+//            Materialize.toast('deactivated', 3000);
+//        }
+//    });
+//});
