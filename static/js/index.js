dcbf82b4e783a5961129f3594c07a88a54898296
Facundo Ramos <facundo@wepay.com>
2017-09-10 01:29:20 -0700

    Fixes login

--- static/js/index.js
+++ static/js/index.js
@@ -1,37 +1,3 @@
 $(function () {
 
-    function checkLoginState() {
-        FB.getLoginStatus(function (response) {
-
-                if (response.status === "connected") {
-
-                    FB.api('/me', function (response) {
-                        $.ajax({
-                            type: 'POST',
-                            url: '/actions/login',
-                            data: JSON.stringify(response),
-                            contentType: "application/json",
-                            dataType: 'json',
-                            success: function (response) {
-                                if (response.redirectUrl) {
-                                    location.href = response.redirectUrl;
-                                }
-                            },
-                            error: function (response) {
-                                if (response.responseJSON.errorMessage) {
-                                    $("#fbError").text(response.responseJSON.errorMessage).show()
-                                }
-                            }
-                        });
-                    })
-                    ;
-                }
-            }
-        );
-
-
-    }
-
-    window.checkLoginState = checkLoginState;
-})
-;
+});
